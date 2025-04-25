1db10918f94d17ce135de2896e21bebeb2adca6a
Nathan Vinicius <limavinicius195@gmail.com>
2025-04-25 15:36:15 -0300

    commit extra

--- desafios.py
+++ desafios.py
@@ -1,5 +1,35 @@
 # Integre na solução anterior um fluxo de While 
+nome_valido = False
+salario_valido = False
+bonus_valido = False
+
+while not nome_valido:
+    try:
+        nome = input("Digite seu nome: ")
+
+        # Verifica se o nome está vazio
+        if len(nome) == 0:
+            raise ValueError("O nome não pode estar vazio.")
+        # Verifica se há números no nome
+        elif any(char.isdigit() for char in nome):
+            raise ValueError("O nome não deve conter números.")
+        else:
+            print("Nome válido:", nome)
+            nome_valido = True
+    except ValueError as e:
+        print(e)
+
+
 # que repita o fluxo até que o usuário insira as 
 # informações corretas
+
+try:
+    salario = float(input("Digite o valor do seu salário: "))
+    if salario < 0:
+        print("Por favor, digite um valor positivo para o salário.")
+except ValueError:
+    print("Entrada inválida para o salário. Por favor, digite um número.")
+    exit()
+
 
 # Solicita ao usuário que digite seu nome
